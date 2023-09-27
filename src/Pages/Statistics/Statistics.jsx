63cefab5102702638f470153b812edccd60e99c0
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-09-27 17:47:51 +0600

    Fixed some issues in the statistics page

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 const Statistics = () => {
@@ -22,11 +22,9 @@ const Statistics = () => {
   );
 
   const pieData = [
-    { name: "My Total Donation", uv: "#000", pv: "#fff", value: total },
+    { name: "My Total Donation", value: total },
     {
       name: "Grand Total Donation",
-      uv: "#000",
-      pv: "#fff",
       value: grandTotal - total,
     },
   ];
@@ -59,28 +57,55 @@ const Statistics = () => {
     );
   };
 
+  const initialPieData = [
+    { name: "My Total Donation", value: 0 },
+    {
+      name: "Grand Total Donation",
+      value: 100,
+    },
+  ];
+
   return (
     <div>
       {data === null ? (
-        <div className="flex flex-col w-full gap-4 md:gap-4 lg:gap-10 mt-10">
-          <div>
-            <img
-              className="mx-auto w-[40%] md:w-[30%] lg:w-[20%]"
-              src="https://i.ibb.co/2gpXhjY/Animation-1695795127636.gif"
-              alt=""
-            />
+        <>
+          <ResponsiveContainer
+            width="100%"
+            height={500}
+            className="mt-15 md:mt-10 lg:mt-10"
+          >
+            <PieChart width={1200} className="mx-auto">
+              <Pie
+                data={initialPieData}
+                dataKey="value"
+                cx="50%"
+                cy="40%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={200}
+                fill="#00C49F"
+              >
+                {data?.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+
+          <div className="yourDonation text-center flex justify-center gap-12">
+            <div className="flex  items-center gap-3">
+              <h1>Your Donation</h1>
+              <div className="h-3 w-10 bg-[#FF444A]"></div>
+            </div>
+            <div className="flex  items-center gap-3">
+              <h1>Total Donation</h1>
+              <div className="h-3 w-10 bg-[#00C49F]"></div>
+            </div>
           </div>
-          <div className="flex flex-col w-full items-center gap-7">
-            <h1 className="text-[23px] md:text-[30px] lg:text-[30px] text-center italic">
-              Looks like you have not made any donations yet
-            </h1>
-            <Link to="/">
-              <button className="btn btn-neutral mx-auto">
-                Start Donating
-              </button>
-            </Link>
-          </div>
-        </div>
+        </>
       ) : (
         <>
           <ResponsiveContainer
@@ -97,7 +122,7 @@ const Statistics = () => {
                 labelLine={false}
                 label={renderCustomizedLabel}
                 outerRadius={200}
-                fill="#8884d8"
+                fill="#00C49F"
               >
                 {data?.map((entry, index) => (
                   <Cell
@@ -108,18 +133,17 @@ const Statistics = () => {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
-          {data?.length > 0 && (
-            <div className="yourDonation text-center flex justify-center gap-12">
-              <div className="flex  items-center gap-3">
-                <h1>Your Donation</h1>
-                <div className="h-3 w-10 bg-[#FF444A]"></div>
-              </div>
-              <div className="flex  items-center gap-3">
-                <h1>Total Donation</h1>
-                <div className="h-3 w-10 bg-[#00C49F]"></div>
-              </div>
+
+          <div className="yourDonation text-center flex justify-center gap-12">
+            <div className="flex  items-center gap-3">
+              <h1>Your Donation</h1>
+              <div className="h-3 w-10 bg-[#FF444A]"></div>
             </div>
-          )}
+            <div className="flex  items-center gap-3">
+              <h1>Total Donation</h1>
+              <div className="h-3 w-10 bg-[#00C49F]"></div>
+            </div>
+          </div>
         </>
       )}
     </div>
